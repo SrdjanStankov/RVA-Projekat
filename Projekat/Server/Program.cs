b1d535d1c12f2fef3a9a3091215cf0fcb8c0a485
Srdjan Stankov <srki.miki@gmail.com>
2019-08-15 13:54:04 +0200

    Fixed timeout error

--- Projekat/Server/Program.cs
+++ Projekat/Server/Program.cs
@@ -14,6 +14,12 @@ namespace Server
 			log.Debug("Server program started");
 			Console.Title = "RVA Projekat - Server";
 			server.Open();
+			Console.WriteLine("Doing some stuff...");
+			using (var ctx = new ModelContext())
+			{
+				
+			}
+			Console.WriteLine("Server ready...");
 			Console.ReadKey(true);
 			server.Close();
 			log.Debug("Server program closed");
